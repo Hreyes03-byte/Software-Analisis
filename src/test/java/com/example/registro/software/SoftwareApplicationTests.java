bd4af4bab93e61b5c5f6c0c40d87036b948c0717
Hugo David Reyes Fuentes <03hugorf03@gmail.com>
2025-06-15 22:18:38 -0500

    Se agrega el proyecto

---
+++ src/test/java/com/example/registro/software/SoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registro.software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
